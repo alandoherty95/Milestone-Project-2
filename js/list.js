c758347171c23a2eba7dbdd77e31d6acf0934fc6
alandoherty95 <alandoherty95@gmail.com>
2021-01-10 20:00:45 +0000

    Added list of sights & cofee

---
+++ js/list.js
@@ -0,0 +1,162 @@
+/*--Below is a list of Sights, Activities and Coffee--*/
+
+let selectionJSON = {
+    /*=-=-=-=-=-=-SIGHT SEEING-=-=-*/
+    // SIGHT 1: King John's Castle
+	KJC: {
+		Id: "1",
+		info: `King John's Castle is a 13th-century castle located on King's Island in Limerick, Ireland, next to the River Shannon. Although the site dates back to 922 when the Vikings lived on the Island, the castle itself was built on the orders of King John in 1200.`
+	},
+	// SIGHT 2: Milk Market
+	MM: {
+		Id: "2",
+		info: `Airy, covered market selling fruits, veggies, meats, dairy items, crafts & baked goods since 1852.`
+	},
+	// SIGHT 3: Treaty Stone
+	SS: {
+		Id: "3",
+		info: `Monument with a stone said to be used in the signing of the 1691 Treaty of Limerick.`
+	},
+	// SIGHT 4: Riverside Walk
+	RW: {
+		Id: "4",
+		info: `This scenic 3.2km shared walking and cycling trail which runs through the historic Shannon Fields and City Centre`
+	},
+	// SIGHT 5: UL Campus
+	ULC: {
+		Id: "5",
+		info: `The UL campus is truly one of the most beautiful university campuses anywhere in the world. Visitors never fail to be impressed by this peaceful green place with its trees, its fountains and the beautiful river Shannon flowing majestically through it all.`
+	},
+	// SIGHT 6: Castletroy Neightbourhood Park 
+	CNP: {
+		Id: "6",
+		info: `Castletroy Neighbourhood Park features 1.2 km of pedestrian pathways, a fully equipped children’s playground, skateboarding area, circular cycle route and central performance/exhibition area with covering structure.`
+	},
+	// SIGHT 7: People's Park
+	PP: {
+		Id: "7",
+		info: `The People's Park is a park located by Pery Square in Limerick, Ireland, just west of the railway station and bus terminal. At the northern edge of the park is the Limerick City Gallery of Art.`
+	},
+	// SIGHT 8: Sarsfield Bridge
+	SB: {
+		Id: "8",
+		info: `Five-span limestone road bridge, built between 1824-35, spanning the River Shannon joining Sarsfield Street and Ennis Road on a southeast/northwest axis, splaying to both sides to form abutments at the junction with the Ennis Road.`
+	},
+
+	// SIGHT 9: 
+	S9: {
+		Id: "9",
+		info: ``
+	},
+	// SIGHT 10:
+	S10: {
+		Id: "10",
+		info: ``
+	},
+
+	//-=-=-=-=-=--=-=-=-ACTIVITES =-=-=--=-=-=--=-=
+
+	// ACTIVITY 1: 
+	A1: {
+		Id: "11",
+		info: ``
+	},
+	// ACTIVITY 2:
+	A2: {
+		Id: "12",
+		info: ``
+	},
+	// ACTIVITY 3:
+	A3: {
+		Id: "13",
+		info: ``
+	},
+	// ACTIVITY 4:
+	A4: {
+		Id: "14",
+		info: ``
+	},
+	// ACTIVITY 5:
+	A5: {
+		Id: "15",
+		info: ``
+	},
+	// ACTIVITY 6:
+	A6: {
+		Id: "16",
+		info: ``
+	},
+	// ACTIVITY 7:
+	A7: {
+		Id: "17",
+		info: ``
+	},
+	// ACTIVITY 8:
+	A8: {
+		Id: "18",
+		info: ``
+	},
+	// ACTIVITY 9:
+	A9: {
+		Id: "19",
+		info: ``
+	},
+	// ACTIVITY 10:
+	A10: {
+		Id: "20",
+		info: ``
+	},
+
+	//-=-=-=-=-=--=-=-=-COFFEE =-=-=--=-=-=--=-=
+
+	// COFFEE 1: Hook & Ladder Sarsfield Street
+	HLSS: {
+		Id: "21",
+		info: `Hook & Ladder “Living Café” offer’s customers a unique experience with a variety of store concepts under one roof – café, cookery school, furniture and home accessories store.`
+	},
+	// COFFEE 2: Costa Coffee
+	CC: {
+		Id: "22",
+		info: `Costa Coffee is the fastest growing coffee shop chain in the world and loved for its naturally sweet, smooth and distinct flavour.`
+	},
+	// COFFEE 3: Gloria Jean's Coffees Limerick
+	GJCL: {
+		Id: "23",
+		info: `Specialty coffeehouse chain offering gourmet blends in a variety of flavors, plus tea & pastries.`
+	},
+	// COFFEE 4: Story Cafe
+	SC: {
+		Id: "24",
+		info: `A coffee shop located in the heart of Limerick on the famous Denmark Street`
+	},
+	// COFFEE 5: Insomnia Coffee Company
+	ICC: {
+		Id: "25",
+		info: `Insomnia Coffee Company is an Irish independent coffee chain.`
+	},
+	// COFFEE 6: Starbucks
+	ST: {
+		Id: "26",
+		info: `Today, with more than 15,000 stores in 50 countries, Starbucks is the premier roaster and retailer of specialty coffee in the world. And with every cup, we strive to bring both our heritage and an exceptional experience to life.`
+	},
+	// COFFEE 7: Arabica Coffee Co.
+	ACC: {
+		Id: "27",
+		info: `Arabica Coffee Roasters strive to provide world class coffee using the latest in coffee brewing technology and carefully sourced beans. `
+	},
+	// COFFEE 8: Café Rosé
+	CR: {
+		Id: "28",
+		info: `Cafe & bistro in the heart of Limerick city serving modern cuisine.`
+	},
+	// COFFEE 9: Sodalicious
+	SO: {
+		Id: "29",
+		info: `Sodalicious bread prides itself on celebrating our wonderful indigenous soda bread by reinventing it with many fresh flavours without destroying the integrity of this original Irish staple.`
+    },
+    // COFFEE 10: Aroma Coffee House
+	ACH: {
+		Id: "30",
+		info: `A cosy coffee shop located on Roches Street`
+	}
+};
